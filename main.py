b551fe0484bdc680c81b154fa31dec565d76b801
Maxim Pyatkin <makspyatkin2107@gmail.com>
2025-04-04 13:22:58 +0300

    oda1

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 
 API_URL = 'https://api.telegram.org/bot'
 API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+API_FOX_URL = 'https://randomfox.ca/floof/'
 BOT_TOKEN = '7695418905:AAGCDe7n_Hxb22wtpzQvFC2970y6wmWyrfU'
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+ERROR_TEXT = 'Пишите fox или cat и вам выдасться картинка данного животного'
 
 offset = -2
 counter = 0
 cat_response: requests.Response
+fox_responce: requests.Response
 cat_link: str
 
 
@@ -21,10 +23,17 @@ while counter < 100:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
+            text = result['message']['text']
             cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            fox_responce = requests.get(API_FOX_URL)
+            if text == 'cat':
+                if cat_response.status_code == 200:
+                    cat_link = cat_response.json()[0]['url']
+                    requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            elif text == 'fox':
+                if fox_responce.status_code == 200:
+                    fox_link = fox_responce.json()['image']
+                    requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={fox_link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
